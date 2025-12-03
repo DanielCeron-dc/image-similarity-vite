a9028dd7888f61752650b1120bf78a52ae6cb722
Andrea Realpe <110572707+andrearealpe@users.noreply.github.com>
2025-12-02 23:07:34 -0500

    style(header): es más minimalista y cambia a un logo distinto de la uni

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,24 +1,35 @@
-import unicaucaLogo from "../../assets/unicauca2.jpeg";
+import unicaucaLogo from "../../assets/unicauca.png";
 
 export function Header() {
   return (
     <header className="px-10 pt-10 pb-8 shrink-0">
-      <div className="flex items-center gap-5">
-        <div className="h-24 w-28 rounded-lg overflow-hidden bg-transparent">
-          <img
-            src={unicaucaLogo}
-            alt="Universidad del Cauca"
-            className="h-full w-full object-contain bg-transparent"
-            loading="lazy"
-          />
-        </div>
-        <div>
-          <h1 className="text-4xl font-black text-white tracking-tight drop-shadow-2xl">
-            Sube la huella
-          </h1>
-          <p className="text-base text-purple-100 mt-1 drop-shadow-lg">Encuentra huellas similares utilizando un sistema SCBIR</p>
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-6">
+          {/* Logo limpio */}
+          <div className="h-25 w-32">
+            <img
+              src={unicaucaLogo}
+              alt="Universidad del Cauca"
+              className="h-full w-full object-contain"
+              loading="lazy"
+            />
+          </div>
+
+          <div>
+            <div className="flex items-baseline gap-4">
+              <h1 className="text-4xl font-black text-white drop-shadow-2xl">
+                SCBIR: Huellas Latentes
+              </h1>
+            </div>
+
+            <div className="mt-2">
+              <p className="text-md font-medium drop-shadow-lg">
+                Sistema de Recuperación por Contenido para Huellas Dactilares
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </header>
-  )
+  );
 }
